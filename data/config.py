89d99d2b77732245132387e854f4ca4c0c914378
Dick Dikins <64678180+FeelmyLife-droid@users.noreply.github.com>
2021-02-04 16:26:53 +0300

    Update config.py
    token delete

--- data/config.py
+++ data/config.py
@@ -2,7 +2,7 @@ import os
 
 
 # BOT_TOKEN = '' #Main_bot
-BOT_TOKEN = '1201963552:AAHsOn56nBBxrG7ybH7ud7ieK8dnaRIs2_E' #FeelmyLIfe
+BOT_TOKEN = '' #FeelmyLIfe
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
